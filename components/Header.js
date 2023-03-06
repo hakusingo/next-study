9806fca63e3695a26e8be6cba96bb921a5ee344e
hakusingo <hakusingo@gmail.com>
2023-03-06 10:47:25 +0900

    ♻️

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header className='h-[60px] bg-teal-400 px-4 leading-[60px] text-white font-bold text-[20px]'>
-      ヘッダーだお
+    <header className=''>
+      <div className="container h-[60px] px-4 sm:px-0 mx-auto bg-teal-400 flex justify-between items-center text-white font-bold">
+        <div className="">
+          <Link
+            href="/"
+            className=''
+          >
+            <h1 className='text-3xl'>
+              Next
+            </h1>
+          </Link>
+        </div>
+        <div className="">
+          <Link
+            href="/about"
+            className='ml-auto'
+          >
+            About
+          </Link>
+        </div>
+      </div>
     </header>
   )
 }
